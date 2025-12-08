5782d8b3e756ebd3251df650a38922acfb7215bc
TheKasman <kasapidisandrew@gmail.com>
2025-12-09 10:09:50 +1100

    question 9 done

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - gets the height of the binary tree
+ * @tree: the tree that's in question
+ * Return: returns the size of the treee, 0 if NULL
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t leftH = 0, rightH = 0;
+
+	if (!tree)
+		return (0);
+
+	leftH = binary_tree_height(tree->left);
+	rightH = binary_tree_height(tree->right);
+
+	if (leftH > rightH)
+		return (leftH + 1);
+	else
+		return (rightH + 1);
+}
